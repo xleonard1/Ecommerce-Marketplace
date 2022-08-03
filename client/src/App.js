db535826758e16a1924d346bce4da8e756fe7fc3
Sherreka <rekadarnb@gmail.com>
2022-08-03 15:29:26 -0400

    got partial styling on image grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import Header from './Components/Header'
+import Header from './Components/Header';
+import Carousel from './Components/Carousel';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Carousel />
     </div>
   );
 }
---
+++ client/src/Components/Carousel.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import {ImageList, ImageListItem, Grid} from '@mui/material';
+
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+};
+
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+      cols: 2
+    },
+  ];
+
+const Carousel = () => {
+    return (
+        <Grid
+            container 
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            // style={{ maxWidth: 1500 }}
+        >
+        <ImageList
+        //   sx={{ width: 500, height: 450 }}
+          variant="quilted"
+          cols={4}
+          rows={2}
+          rowHeight={121}
+        >
+          {itemData.map((item) => (
+            <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+              <img
+                {...srcset(item.img, 121, item.rows, item.cols)}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        </Grid>
+      ); 
+};
+
+export default Carousel;
