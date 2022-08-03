3d7fc7eda1a579db95cbbc41b1cfbb5faee1a888
Xavier Leonard <xavier.lnrd@gmail.com>
2022-08-02 21:13:21 -0400

    user and product models added (#3)
    Co-authored-by: Bhavna Jain <bhavnaostwal@gmail.com>

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  
+});
+
+
+
+
+
+module.exports = Mongoose.model('User', UserSchema);
+
