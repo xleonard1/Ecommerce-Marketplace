bd7d8ebb6af6febfb23d8d2fa9eb88193b0c60b6
Sherreka <rekadarnb@gmail.com>
2022-08-03 10:15:38 -0400

    added buttons

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,124 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Toolbar,
+  styled,
+  Typography,
+  InputBase,
+  Badge,
+  IconButton,
+} from "@mui/material";
+import {
+  ShoppingCart,
+  Menu,
+  LocalShipping,
+  CreditCard,
+  LocalPhone,
+} from "@mui/icons-material";
+import React, { useState } from "react";
+
+const TopBar = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
+  background: "black",
+  height: "50px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "30%",
+}));
+
+const Icons = styled("div")(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <AppBar position="sticky" style={{ background: "red" }}>
+      <TopBar>
+        <Badge>
+          <LocalShipping style={{ marginRight: "8px" }} />
+          <Typography>Free Shipping</Typography>
+        </Badge>
+        <Badge>
+          <CreditCard style={{ marginRight: "8px" }} />
+          <Typography>Payment Methods</Typography>
+        </Badge>
+        <Badge>
+          <LocalPhone style={{ marginRight: "8px" }} />
+          <Typography>222-222-2222</Typography>
+        </Badge>
+      </TopBar>
+      <StyledToolbar>
+        <Box sx={{ display: "flex" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography
+            variant="h6"
+            sx={{ display: { xs: "none", sm: "block", alignSelf: "center" } }}
+          >
+            E-COMMERCE MARKETPLACE
+          </Typography>
+        </Box>
+        <Search>
+          <InputBase placeholder="Search Products" />
+        </Search>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between'}}>
+        <Icons>
+          <Badge badgeContent={1}>
+            <ShoppingCart />
+          </Badge>
+        </Icons>
+        <Button
+          variant="text"
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          open={open}
+          onClose={(e) => setOpen(false)}
+          anchororigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          transformorigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <Typography>Brands</Typography>
+        </Button>
+        <Button><Typography>Shop</Typography></Button>
+        <Button><Typography>Welcome</Typography></Button>
+        </Box>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
