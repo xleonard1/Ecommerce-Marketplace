aff6a587939a4eb4ed0342da4fb987c5a2184d8d
Bhavna Jain <bhavnaostwal@gmail.com>
2022-08-02 21:35:02 -0400

    added typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,33 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
-    _id: ID!
-    name: String!
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
   }
 
-  type Matchup {
+  type Product {
     _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+    name: String
+    imageUrl: String
+    description: String
+    price: Int
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    products: [Product]
+    product(name: String!): Product    
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addProduct(name: String!, imageUrl: String, description: String!, price:Int): Product
+    
   }
+  
 `;
 
 module.exports = typeDefs;
