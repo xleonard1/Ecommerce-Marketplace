0624202a6557aa07ab36cc57e67775080a1a0c1b
Sherreka <rekadarnb@gmail.com>
2022-08-03 16:42:34 -0400

    adding hyperlinks

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -7,18 +7,40 @@ const Footer = () => {
       <Box>
         <Container maxWidth="lg">
           <Grid container spacing={5}>
-            <Grid>
+            <Grid item xs={12} sm={4}>
               <Box borderBottom={1}>Help</Box>
               <Box>
                 <Link href="/">Contact</Link>
               </Box>
-              <Box borderBottom={1}>Help</Box>
               <Box>
                 <Link href="/">Support</Link>
               </Box>
-              <Box borderBottom={1}>Help</Box>
               <Box>
                 <Link href="/">Privacy Policy</Link>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Account</Box>
+              <Box>
+                <Link href="/">Login</Link>
+              </Box>
+              <Box>
+                <Link href="/">Register</Link>
+              </Box>
+              <Box>
+                <Link href="/">Privacy</Link>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Messages</Box>
+              <Box>
+                <Link href="/">Backup</Link>
+              </Box>
+              <Box>
+                <Link href="/">History</Link>
+              </Box>
+              <Box>
+                <Link href="/">Roll</Link>
               </Box>
             </Grid>
           </Grid>
