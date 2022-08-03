6dc27083f442b9a95755d7fe06604c841c4d2e74
Sherreka <rekadarnb@gmail.com>
2022-08-03 16:55:24 -0400

    adding styling and background color to footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,46 +1,47 @@
 import * as React from "react";
-import { Container, Grid, Box, Link, Divider } from "@mui/material";
+import { Container, Grid, Box, Link } from "@mui/material";
+
 
 const Footer = () => {
   return (
     <footer>
-      <Box>
+      <Box bgcolor='black' px={{ xs: 3, sm: 10}} py={{ xs: 5, sm: 10}} color='white'>
         <Container maxWidth="lg">
           <Grid container spacing={5}>
             <Grid item xs={12} sm={4}>
               <Box borderBottom={1}>Help</Box>
               <Box>
-                <Link href="/">Contact</Link>
+                <Link href="/" color='inherit'>Contact</Link>
               </Box>
               <Box>
-                <Link href="/">Support</Link>
+                <Link href="/" color='inherit'>Support</Link>
               </Box>
               <Box>
-                <Link href="/">Privacy Policy</Link>
+                <Link href="/" color='inherit'>Privacy Policy</Link>
               </Box>
             </Grid>
             <Grid item xs={12} sm={4}>
               <Box borderBottom={1}>Account</Box>
               <Box>
-                <Link href="/">Login</Link>
+                <Link href="/" color='inherit'>Login</Link>
               </Box>
               <Box>
-                <Link href="/">Register</Link>
+                <Link href="/" color='inherit'>Register</Link>
               </Box>
               <Box>
-                <Link href="/">Privacy</Link>
+                <Link href="/" color='inherit'>Privacy</Link>
               </Box>
             </Grid>
             <Grid item xs={12} sm={4}>
               <Box borderBottom={1}>Messages</Box>
               <Box>
-                <Link href="/">Backup</Link>
+                <Link href="/" color='inherit'>Backup</Link>
               </Box>
               <Box>
-                <Link href="/">History</Link>
+                <Link href="/" color='inherit'>History</Link>
               </Box>
               <Box>
-                <Link href="/">Roll</Link>
+                <Link href="/" color='inherit'>Roll</Link>
               </Box>
             </Grid>
           </Grid>
