3fc898873767f0320d2a9bf46cd4aa42a0d88f87
Michael Schenck <99237120+mdschenck@users.noreply.github.com>
2022-08-02 20:38:26 -0700

    Adding UI Pages (#7)

---
+++ client/src/Components/Shipping.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+<h1>Shipping</h1>
+
+<p> All items ship with USPS Priority Mail or FedEx 2-day air to ensure delivery to your location within 2 business days. Shipping costs are included in prices so there are no surprises! You will recieve a confirmation email with your shipping tracking information as soon as your item ships.</p> 
+
