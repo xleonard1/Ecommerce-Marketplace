edc4bcc8dcf05de03036a1c455f9ba10ce2d0a63
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-08-02 20:37:08 -0700

    Adding UI Pages

---
+++ client/src/Components/Product.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+return (
+
+`<h1>${product.title}</h1>
+<img src="${product.imageUrl}">
+<p> ${product.description}</p>
+<p> ${product.category}</p>
+<p> ${product.price}</p>`
+
+);
