20475248cf45570a19d0541a3eed406fef5bfd87
Michael Schenck <99237120+mdschenck@users.noreply.github.com>
2022-08-04 17:19:58 -0700

    Product Page , Contact Us Page and Shipping Details Page (#15)

--- client/src/Components/Contact.jsx
+++ client/src/Components/Contact.jsx
@@ -13,5 +13,90 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-<h1>Contact Us</h1>
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+}
 
+const theme = createTheme();
+
+
+const contact = () => {
+
+return (
+<ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+        <Row>
+            <Typography component="h1" variant="h5">
+            Contact Us
+            </Typography>
+        </Row>
+        <Row>
+            <Col xs={12} md={6}>
+
+                <p>Our trained support staff are on call 24/7 to assist with any issues you may have.</p>
+                <p>Phone: 404-123-4567</p>
+                <p>Email: 
+                <a href="mailto:customerservice@ecommercemarketplace.com"> customerservice@ecommercemarketplace.com </a></p>
+                <h3>Shipping Questions:<h3>
+                <p>
+                Visit our shipping FAQ page <a href="/shipping" target="top">here</a>
+                </p>
+            </Col>
+
+            <Col xs={12} md={6}>
+                <h2>Ask Ecommerce Marketplace A Question</h2>
+                    <Form>
+                        <Form.Label>Email</Form.Label>
+                        <InputGroup hasValidation>
+                            <Form.Control type="email" required isInvalid/>
+                            <Form.Control.Feedback type="invalid">
+                            Please enter a valid email address.
+                            </Form.Control.Feedback>
+                        </InputGroup>
+
+                        <Form.Group className="mb-3" controlId="formContactBody">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control type="text" placeholder="Please enter your message here" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit" className="right">
+                        Submit
+                        </Button>
+                    </Form>
+            </Col>
+        </Row>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
+</ThemeProvider>
+)
+};
+
+export default contact;
+
+
+)
+};
+
+export default contact;
--- client/src/Components/Shipping.jsx
+++ client/src/Components/Shipping.jsx
@@ -10,9 +10,20 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const shipping = () => {
 
+return(
 
+<Container maxWidth="sm">
+<Row>
 <h1>Shipping</h1>
-
+</Row>
+<Row>
+    <img src="/assets/images/truck.PNG"></img><img src="/assets/images/fedexLogo.PNG"></img><img src="/assets/images/uspsLogo.PNG"></img>
+</Row>
+<Row>
 <p> All items ship with USPS Priority Mail or FedEx 2-day air to ensure delivery to your location within 2 business days. Shipping costs are included in prices so there are no surprises! You will recieve a confirmation email with your shipping tracking information as soon as your item ships.</p> 
+</Row>
 
+)
+};
