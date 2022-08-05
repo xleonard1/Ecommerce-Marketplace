9733f5d50576f17ff4469f0c59482196ac31109b
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-08-04 22:49:52 -0700

    Refactor pages into proper Material UI syntax - Product, Mission & Contact complete

--- client/src/Components/Mission.jsx
+++ client/src/Components/Mission.jsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -27,16 +24,18 @@ const mission = () => {
                   }}
                 >
                   
-                <Row>
-                    <Typography component="h1" variant="h5">
-                    Our Mission
-                    </Typography>
-                </Row>
-                <Row>
-                    <p>
-                      Our mission is to create the best online marketplace, where shoppers and sellers are able to purchase truly world class, hard to find products from our network of reputable sellers.  Customer satisfaction is 100% guaranteed, so you can rest assured that if any issues arise during your shopping experience on Ecommerce Marketplace that it will be taken care of.  
-                    </p> 
-                </Row>
+                  <Grid container spacing={1}>
+                        <Grid item xs={12}>
+                          <Typography component="h1" variant="h2">
+                            Our Mission
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <p>
+                          Our mission is to create the best online marketplace, where shoppers and sellers are able to purchase truly world class, hard to find products from our network of reputable sellers.  Customer satisfaction is 100% guaranteed, so you can rest assured that if any issues arise during your shopping experience on Ecommerce Marketplace that it will be taken care of.  
+                        </p> 
+                        </Grid>
+                      </Grid>
                 </Box>
 
             </Container>
