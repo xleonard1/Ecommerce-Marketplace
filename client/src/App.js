2261ca1bd0530bd9580fd4e9a2566fde643721ed
Michael Schenck <99237120+mdschenck@users.noreply.github.com>
2022-08-04 22:53:03 -0700

    Refactor pages into proper Material UI syntax - Product, Mission & Contact complete (#23)

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,34 @@ import Header from './Components/Header';
 // import Carousel from './Components/Carousel';
 // import Price from './Components/Price';
 import Rating from './Components/Rating';
-import Footer from './Components/Footer';
-import Product from './Components/Product';
+// import Footer from './Components/Footer';
+
+// **Completed Basic Pages**
+// import Product from './Components/Product';
 // import Mission from './Components/Mission';
+// import Careers from './Components/Careers';
+
+// **In Progress Pages**
 // import Contact from './Components/Contact';
 // import Returns from './Components/Returns';
 // import Shipping from './Components/Shipping';
-// import Careers from './Components/Careers';
+// import ProductForm from './Components/ProductForm';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Product />
       {/* <Carousel /> */}
       {/* <Price /> */}
-      {/* <Rating /> */}
-      <Footer />
+      <Rating />
+
+      {/* <Product /> */}
+      {/* <Careers /> */}
+      {/* <Mission /> */}
+
+      {/* <Footer /> */}
     </div>
   );
 }
--- client/src/Components/Careers.jsx
+++ client/src/Components/Careers.jsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import careerImg from '../images/careers.png';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
@@ -20,6 +18,8 @@ const careers = () => {
                 <CssBaseline />
                 <Box
                   sx={{
+                    
+                    minWidth: 600,
                     marginTop: 8,
                     display: 'flex',
                     flexDirection: 'column',
@@ -27,16 +27,19 @@ const careers = () => {
                   }}
                 >
                     
-                <Row>
-                    <Typography component="h1" variant="h5">
-                    Careers
-                    </Typography>
-                </Row>
-                <Row>
-                    <p>
-                    Ecommerce is a booming business and we are always looking for excellent candidates to join our expanding team of industry professionals. We take pride in being an inclusive workplace with a fun, fast moving culture of positive momentum and employee growth.  Reach out to us if you think you'd be a good fit for our team! We can't wait to hear from you.    
-                    </p> 
-                </Row>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12}>
+                            <Typography component="h1" variant="h2">
+                            Careers
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <img src={careerImg} alt="careers" />
+                            <p>
+                            Ecommerce is a booming business and we are always looking for excellent candidates to join our expanding team of industry professionals. We take pride in being an inclusive workplace with a fun, fast moving culture of positive momentum and employee growth.  Reach out to us if you think you'd be a good fit for our team! We can't wait to hear from you.    
+                            </p> 
+                        </Grid>
+                    </Grid>
                 </Box>
 
             </Container>
