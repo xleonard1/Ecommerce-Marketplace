74f8e3d08ae43e9154acd54ff47715cb8caf37a7
Michael Schenck <99237120+mdschenck@users.noreply.github.com>
2022-08-06 08:51:33 -0700

    Added Contact Us, Product Form Page, Shipping, and Mission - All pages displaying properly (#26)

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import Header from './Components/Header';
-// import Carousel from './Components/Carousel';
+import Carousel from './Components/Carousel';
 // import Price from './Components/Price';
 import Rating from './Components/Rating';
 // import Footer from './Components/Footer';
 
-// **Completed Basic Pages**
-// import Product from './Components/Product';
-// import Mission from './Components/Mission';
-// import Careers from './Components/Careers';
-
-// **In Progress Pages**
-// import Contact from './Components/Contact';
-// import Returns from './Components/Returns';
-// import Shipping from './Components/Shipping';
-// import ProductForm from './Components/ProductForm';
+import Product from './Components/Product';
+import Mission from './Components/Mission';
+import Careers from './Components/Careers';
+import Contact from './Components/Contact';
+import Returns from './Components/Returns';
+import Shipping from './Components/Shipping';
+import ProductForm from './Components/ProductForm';
 
 import './App.css';
 
@@ -21,14 +18,20 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Carousel /> */}
+
+{/* --LEFT SIDE PAGE STATIC COMPONENTS-- */}
       {/* <Price /> */}
       <Rating />
 
+{/* --MAIN CONTENT PAGE COMPONENTS-- */}
+      {/* <Carousel /> */}
       {/* <Product /> */}
+      {/* <ProductForm /> */}
+      {/* <Contact /> */}
       {/* <Careers /> */}
       {/* <Mission /> */}
-
+      {/* <Shipping /> */}
+      {/* <Returns /> */}
       {/* <Footer /> */}
     </div>
   );
--- client/src/Components/Returns.jsx
+++ client/src/Components/Returns.jsx
@@ -8,6 +8,7 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import returnImg from '../images/returns.png';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
@@ -26,24 +27,26 @@ const returns = () => {
                     alignItems: 'center',
                   }}
                 >
+                  <Grid container spacing={1}>
+                    <Grid item xs={12}>
+                      <Typography     component="h1" variant="h2">
+                      Return Policy
+                      </Typography>
+                    </Grid>
 
-                <Row>
-                    <Typography component="h1" variant="h5">
-                    Return Policy
-                    </Typography>
-                </Row>
-                <Row>
-                    <p>
+                    <Grid item xs={12}>
+                      <img src={returnImg} alt="Returns" />
+                      <p>
                       Our return policy is simple, if your item is damaged or arrives not in the condition advertised, you will be able to return your item or exchange it for a replacement at no charge to you.  
-                    </p> 
-                    <p>
+                      </p> 
+                      <p>
                       In the event that what you recieve does not match the item you ordered, reach out to our customer service team and they will provide you with a return authorization (RMA) and send you a postage-paid return shipping label. If a replacement is not desired, you will be refunded the full amount of your purchase once the return is recieved at our return processing center. 
-                    </p>
-                    <p><a href="mailto:customerservice@ecommercemarketplace.com"> Email Us </a> to inquire about a return</p>
+                      </p>
+                      <p><a href="mailto:customerservice@ecommercemarketplace.com"> Email Us </a> to inquire about a return</p>
 
-                </Row>
+                    </Grid>
+                  </Grid>
                 </Box>
-
             </Container>
         </ThemeProvider>
 
