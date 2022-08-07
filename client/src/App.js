51f86587adcc79347e49e5e177850eabba122fc4
Michael Schenck <99237120+mdschenck@users.noreply.github.com>
2022-08-07 11:04:34 -0700

    Add react router (#30)
    * Adding Pages directory, home page
    
    * Adding pages directory & home page
    
    * Add appolo client to package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// --Import Components--
+
 import Header from './Components/Header';
 import Carousel from './Components/Carousel';
 // import Price from './Components/Price';
 import Rating from './Components/Rating';
 import Footer from './Components/Footer';
-import Product from './Components/Product';
-import Mission from './Components/Mission';
-import Careers from './Components/Careers';
-import Contact from './Components/Contact';
-import Returns from './Components/Returns';
-import Shipping from './Components/Shipping';
-import ProductForm from './Components/ProductForm';
+
+// --Import Pages--
+
+// import Home from './Pages/Home';
+import Product from './Pages/Product';
+import Mission from './Pages/Mission';
+import Careers from './Pages/Careers';
+import Contact from './Pages/Contact';
+import Returns from './Pages/Returns';
+import Shipping from './Pages/Shipping';
+import ProductForm from './Pages/ProductForm';
+// import Login from './Pages/Login';
+// import Dashboard from './Pages/Dashboard';
 
 import './App.css';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
