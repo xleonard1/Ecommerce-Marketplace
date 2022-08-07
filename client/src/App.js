7333305aea401413cc440407562f9e9a8f475dcd
Michael Schenck <99237120+mdschenck@users.noreply.github.com>
2022-08-07 11:52:58 -0700

    React router2 (#31)
    * Added React Router Links - sucessfully routing to pages in URL
    
    * added comments

--- client/src/App.js
+++ client/src/App.js
@@ -32,24 +32,99 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
 
-{/* --LEFT SIDE PAGE STATIC COMPONENTS-- */}
+<ApolloProvider client={client}>
+  <Router>
+    <div className="App flex-column justify-flex-start min-100-vh">
+      <Header />
+      <div className="container">
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+
+
+{/*   --COMPONENTS-- */}
       {/* <Price /> */}
-      <Rating />
+      {/* <Rating /> */}
+      {/* <Carousel /> */}
 
 {/* --MAIN CONTENT PAGE COMPONENTS-- */}
-      <Carousel />
-      <Product />
-      <ProductForm />
-      <Contact />
-      <Careers />
-      <Mission />
-      <Shipping />
-      <Returns />
-      <Footer />
+          {/* Define a default route that will render the Home component */}
+          <Route 
+            path="/" 
+            element={<Product />} 
+          />
+
+      {/* Define a route that will take in variable data */}
+      {/* USE THIS ROUTE TO SHOW INDIVIDUAL PRODUCT BY ID */}
+      {/* <Route 
+      path="/products/:productId" 
+      element={<Product />} 
+      /> */}
+
+      {/* --Example product route - delete once variable product is set up-- */}
+      <Route 
+      path="/product" 
+      element={<Product />} 
+      />
+
+
+      <Route 
+      path="/add-product" 
+      element={<ProductForm />} 
+      />
+
+
+      {/* <Route 
+      path="/login" 
+      element={<Login />} 
+      /> */}
+
+
+      <Route 
+      path="/contact" 
+      element={<Contact />} 
+      />
+
+      <Route 
+      path="/careers" 
+      element={<Careers />} 
+      />
+
+      <Route 
+      path="/mission" 
+      element={<Mission />} 
+      />
+
+      {/* <Route 
+      path="/sell-with-us" 
+      element={<SellWithUs />} 
+      /> */}
+
+      <Route 
+      path="/shipping" 
+      element={<Shipping />} 
+      />
+
+      <Route 
+      path="/returns" 
+      element={<Returns />} 
+      />
+
+      {/* <Route 
+      path="/" 
+      element={<Oops404 />} 
+      /> */}
+
+
+      </Routes>
     </div>
+    <div>
+      <Footer /> 
+    </div>
+
+    </div>
+    </Router>
+    </ApolloProvider>
   );
 }
 
