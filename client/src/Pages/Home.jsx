102748369541b0dbeb26948d3b65bc5c5218e7a1
Michael Schenck <99237120+mdschenck@users.noreply.github.com>
2022-08-13 02:24:24 -0700

    Single page app1 (#61)
    * change react version for dependencies
    
    * Added --legacy-peer-deps to install command
    
    * Error in product form
    
    * error in product page
    
    * adding node version to package.json in root
    
    * product form error
    
    * heroku error
    
    * add favicon
    
    * C:/Program Files/Git/ route in server/server.js
    
    * password change
    
    * heroku routes
    
    * add static.json to root to enable react router on heroku
    
    * React router changes
    
    * add react router syntax for heroku
    
    * tweak
    
    * remove IndexRoute
    
    * fix home route
    
    * adding BrowserRouter to index.js
    
    * import browser router
    
    * tweak browser router & react router
    
    * typo
    
    * import render from react-dom?
    
    * import browser router again
    
    * remove strictmode
    
    * remove browser router
    
    * Convert to single page for Heroku deploy
    
    * deactivate unnecessary single page functions
    
    * Heroku deploy successful

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,14 +1,75 @@
 // import { useQuery } from '@apollo/client';
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Carousel from '../Components/Carousel';
+import  {useEffect} from "react";
+import { Grid, Container, CircularProgress } from "@mui/material";
+import Box from '@mui/material/Box';
+import ProductCard from '../Components/ProductCard';
+import Price from '../Components/Price';
+import Rating from '../Components/Rating';
+import { spacing } from '@mui/system'
+import { useQuery } from "@apollo/client";
+import {QUERY_ALL_PRODUCTS} from '../utils/queries';
+import { useStoreContext } from "../utils/GlobalState";
+import {
+  UPDATE_PRODUCTS,
+} from "../utils/actions";
+import Login from "../Pages/Login";
+
+
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { useState } from 'react'
 
 const theme = createTheme();
 
+
+export function SignIn() {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [login, { error }] = useMutation(LOGIN);
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+}
 const Home = () => {
+  const {loading, data} = useQuery(QUERY_ALL_PRODUCTS)
+  const [state, dispatch] = useStoreContext()
+
+  useEffect(() => {
+      if (!loading && data) {
+          console.log('data', data)
+          dispatch({
+              type: UPDATE_PRODUCTS,
+              products: data.products
+          })
+      }
+  }, [loading, data])
+  console.log('data', data)
 
   return (
     <ThemeProvider theme={theme}>
@@ -17,9 +78,7 @@ const Home = () => {
     <main>
       <div className="flex-row justify-center">
        <Carousel />
-       
-
-
+      
       </div>
     </main>
   </Container>
