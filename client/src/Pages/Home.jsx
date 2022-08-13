4c84258721dc56e95565a17726175bbd78784186
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-08-13 02:16:22 -0700

    deactivate unnecessary single page functions

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -88,7 +88,7 @@ const Home = () => {
        <Carousel />
       
       </div>
-      <Box
+      {/* <Box
                   sx={{
                     marginTop: 5,
                     display: 'flex',
@@ -118,80 +118,7 @@ const Home = () => {
                 ))}
                 </Grid>
             </Grid>
-            </Box>
-          {/* <Button onClick={handleOpen}>Login</Button> */}
-          {/* <Modal
-          open={open}
-          onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          > */}
-<Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" 
-          // onSubmit={handleSubmit} 
-          noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              // onChange={handleChange}
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              // onChange={handleChange}
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-          {/* </Modal> */}
+            </Box> */}
     </main>
   </Container>
 </ThemeProvider>
